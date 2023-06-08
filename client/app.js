b2d3bd6229fd4bf999e1c2ee388355384f2d1ed8
OhWhale515 <sterlinggreen515@gmail.com>
2023-06-08 16:56:26 -0400

    server and client updates

--- client/app.js
+++ client/app.js
@@ -1,55 +1,56 @@
-const { app, BrowserWindow, ipcMain } = require('electron');
+const { app, BrowserWindow, ipcMain } = require('electron')
 const { v4: uuidv4 } = require('uuid');
-const socket = require('socket.io-client')('http://0.0.0.0:5000/view');
 const screenshot = require('screenshot-desktop');
 
-let interval;
+var socket = require('socket.io-client')('http://192.168.0.36:5000');
+var interval;
 
-function createWindow() {
-  const win = new BrowserWindow({
-    width: 500,
-    height: 150,
-    webPreferences: {
-      nodeIntegration: true,
-    },
-  });
-  win.removeMenu();
-  win.loadFile('index.html');
+function createWindow () {
+    const win = new BrowserWindow({
+        width: 500,
+        height: 150,
+        webPreferences: {
+            nodeIntegration: true
+        }
+    })
+    win.removeMenu();
+    win.loadFile('index.html')
 }
 
-app.whenReady().then(createWindow);
+app.whenReady().then(createWindow)
 
 app.on('window-all-closed', () => {
-  if (process.platform !== 'darwin') {
-    app.quit();
-  }
-});
+    if (process.platform !== 'darwin') {
+        app.quit()
+    }
+})
 
 app.on('activate', () => {
-  if (BrowserWindow.getAllWindows().length === 0) {
-    createWindow();
-  }
-});
+    if (BrowserWindow.getAllWindows().length === 0) {
+        createWindow()
+    }
+})
 
-ipcMain.on('start-share', function (event, arg) {
-  const uuid = uuidv4();
-  socket.emit('join-message', uuid);
-  event.reply('uuid', uuid);
+ipcMain.on("start-share", function(event, arg) {
 
-  interval = setInterval(function () {
-    screenshot().then((img) => {
-      const imgStr = Buffer.from(img).toString('base64');
+    var uuid = uuidv4();
+    socket.emit("join-message", uuid);
+    event.reply("uuid", uuid);
 
-      const obj = {
-        room: uuid,
-        image: imgStr,
-      };
+    interval = setInterval(function() {
+        screenshot().then((img) => {
+            var imgStr = new Buffer(img).toString('base64');
 
-      socket.emit('screen-data', JSON.stringify(obj));
-    });
-  }, 100);
-});
+            var obj = {};
+            obj.room = uuid;
+            obj.image = imgStr;
 
-ipcMain.on('stop-share', function (event, arg) {
-  clearInterval(interval);
-});
+            socket.emit("screen-data", JSON.stringify(obj));
+        })
+    }, 100)
+})
+
+ipcMain.on("stop-share", function(event, arg) {
+
+    clearInterval(interval);
+})
--- server/app.js
+++ server/app.js
@@ -1,33 +1,72 @@
 const express = require('express');
 const app = express();
-const http = require('http').createServer(app);
+const http = require('http').Server(app);
 const io = require('socket.io')(http);
-const screenshot = require('screenshot-desktop');
+const { v4: uuidv4 } = require('uuid');
 
-// Serve the client-side code
+const connectedClients = {}; // Keep track of connected clients
+
 app.use(express.static('public'));
 
-// Handle socket connections
-io.on('connection', (socket) => {
+io.on('connection', function(socket) {
   console.log('A user connected');
 
-  socket.on('join-message', (room) => {
+  socket.on('join-message', function(room) {
+    console.log('User joined room:', room);
     socket.join(room);
-    console.log(`User joined room: ${room}`);
+
+    // Store the client's socket in the connectedClients object
+    connectedClients[socket.id] = { room };
+
+    // Emit an event to notify all connected clients about the updated list of available systems
+    io.emit('available-systems', getAvailableSystems());
   });
 
-  socket.on('disconnect', () => {
-    console.log('A user disconnected');
-  });
-
-  socket.on('screen-data', (data) => {
+  socket.on('screen-data', function(data) {
     io.to(data.room).emit('screen-data', data.image);
   });
 
+  socket.on('disconnect', function() {
+    console.log('A user disconnected');
+
+    // Remove the client's socket from the connectedClients object
+    delete connectedClients[socket.id];
+
+    // Emit an event to notify all connected clients about the updated list of available systems
+    io.emit('available-systems', getAvailableSystems());
+  });
+
+  socket.on('request-connection', function(data) {
+    const { requester, target } = data;
+
+    // Check if the target system is available
+    if (isSystemAvailable(target)) {
+      // Emit an event to the target system to establish the connection
+      io.to(target).emit('connection-request', requester);
+    }
+  });
+
+  socket.on('accept-connection', function(data) {
+    const { requester, target } = data;
+
+    // Emit an event to the requester system to confirm the connection
+    io.to(requester).emit('connection-accepted', target);
+  });
 });
 
-// Start the web server
+function getAvailableSystems() {
+  // Extract the unique room IDs from connectedClients
+  const availableSystems = [...new Set(Object.values(connectedClients).map(client => client.room))];
+  availableSystems.push('Server'); // Add 'Server' as an available system
+  return availableSystems;
+}
+
+function isSystemAvailable(room) {
+  // Check if the specified room is available in connectedClients
+  return Object.values(connectedClients).some(client => client.room === room);
+}
+
 const port = 5000;
-http.listen(port, () => {
-  console.log(`Server running on port ${port}`);
+http.listen(port, function() {
+  console.log('Server listening on port ' + port);
 });
